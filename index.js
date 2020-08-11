0a11ba7acec0ad305c08990809c9a0002ae1a1cb
hansenjl <jenn.leigh.hansen@gmail.com>
2020-08-11 09:02:08 -0700

    finish study group

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const searchInput = document.getElementById('search-input')
 const submitButton = document.getElementById('submit')
+const titleList = document.getElementById('list')
 const baseURL = "https://www.googleapis.com/books/v1/volumes?q="
 //https://www.googleapis.com/books/v1/volumes?q=search+term+here
 
@@ -7,14 +8,32 @@ submitButton.addEventListener('click', handleSubmit)
 
 function handleSubmit(event){
   let searchTerm = searchInput.value.split(" ").join("+")
+
   makeAPICall(searchTerm)
 }
 
 function makeAPICall(searchTerm){
   //write code here
+  console.log("in the make API call function", searchTerm)
+  fetch(baseURL + searchTerm)
+    .then((response) => response.json())
+    .then(addBooksToDom)
+    .catch((error) => error)
+
+    // .then((res) => {
+    //   addBooksToDom(res)
+    // })
 
 }
 
-function addBooksToDom(){
+function addBooksToDom(response){
    //write code here
+   console.log("in add books to dom")
+   console.log(response)
+   if(response.error){
+    titleList.innerHTML = `<li>${response.error.message}</li>`
+   }
+   const titles = response["items"].map(item => item.volumeInfo.title)
+   const liElements = titles.map(title => `<li class="title">${title}</li>`)
+   titleList.innerHTML = liElements.join('')
 }
--- nonblocking.js
+++ nonblocking.js
@@ -4,10 +4,13 @@ setTimeout(function(){
   console.log("Hi...")
 }, 1000)
 
-console.log("*Rubs eyes*")
 
-console.log("*Yawns*")
 
 setTimeout(function(){
   console.log("Just waking up...")
 }, 2000)
+
+
+console.log("*Rubs eyes*")
+
+console.log("*Yawns*")
